a35507b70ce941bf63b179a29d56b5c7760381d0
chamisupu <chami2170@gmail.com>
2023-02-20 23:28:07 +0530

    DB connection

---
+++ src/main/java/com/mycompany/service/DBConnection.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.service;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author A V I A N
+ */
+public class DBConnection {
+    
+        private static final String DRIVERNAME = "com.mysql.jdbc.Driver";
+	private static final String USERNAME = "root";
+	private static final String PASSWORD = "12345678";	
+	private static final String CONNECTION_URL = "jdbc:mysql://localhost:3306/bumblebee?autoReconnect=true&useSSL=false";
+	
+	private static Connection connection;
+	
+	public static Connection getConnection() {
+		
+		try {
+			
+			// loading the driver
+			Class.forName(DRIVERNAME);
+			
+			// getting the connection
+			connection = DriverManager.getConnection(CONNECTION_URL, USERNAME, PASSWORD);
+			
+		} catch (ClassNotFoundException e) {
+			
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		} catch (SQLException e) {
+			
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
+		
+		return connection;
+	}
+    
+}
