c507f70b1194fd88a4f5c8f3750fd5b3d0d5fd5d
chamisupu <chami2170@gmail.com>
2023-03-06 12:44:48 +0530

    Back-end part 2

---
+++ src/main/java/com/mycompany/service/Controllers/OrderController.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.service.Controllers;
+
+import com.mycompany.service.DBConnection;
+import com.mycompany.service.Models.Orders;
+import java.sql.Connection;
+import java.sql.Statement;
+
+/**
+ *
+ * @author A V I A N
+ */
+public class OrderController {
+    
+    public boolean customerOrder(Orders order){
+        
+        Connection connection = DBConnection.getConnection();
+        
+        try{
+         
+            Statement stam = connection.createStatement();
+            int rows = stam.executeUpdate((" INSERT INTO `order`(`id`,"
+              + " `name`, `address`, `price`, `loan`, `birthday`, `plan`) VALUES ('"+order.getOrder_id()+"','"
+            +order.getOrder_name()+"','"+order.getOrder_address()+"','"+order.getOrder_price()+"','"+order.getOrder_loan()+"','"+order.getOrder_birthday()+"','"+order.getOrder_plan()+"')"));
+            
+           return rows>0;
+            
+        }catch(Exception ex){
+            System.out.println("ERROR"+" "+ex);
+        }
+         
+        return false;
+    }
+}
---
+++ src/main/java/com/mycompany/service/Services/OrderService.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/WebServices/WebService.java to edit this template
+ */
+package com.mycompany.service.Services;
+
+
+import com.mycompany.service.Controllers.OrderController;
+import com.mycompany.service.Models.Orders;
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+
+/**
+ *
+ * @author A V I A N
+ */
+@WebService(serviceName = "OrderService")
+public class OrderService {
+
+    /**
+     * This is a sample web service operation
+     */
+    @WebMethod(operationName = "customerOrder")
+    public boolean Cus_ord(@WebParam(name="order")Orders order) {
+        OrderController ord_controller = new OrderController();
+        return ord_controller.customerOrder(order);
+       
+    }
+}
